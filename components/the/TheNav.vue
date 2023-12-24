1708f8b280998a5b2d221fd9372e0c140a6b974c
cernochmartin <cernochm@outlook.cz>
2023-12-24 20:22:23 +0100

    fix: style change

--- components/the/TheNav.vue
+++ components/the/TheNav.vue
@@ -8,7 +8,7 @@ async function logout() {
 }
 </script>
 <template>
-    <nav class="flex bg-prussian text-white justify-between h-16 text-xl font-semibold items-center px-96">
+    <nav class="flex bg-sky text-white justify-between h-16 text-xl font-semibold items-center px-96">
         <NuxtLink to="/">
             <img src="~/assets/icon/TeacherDigitalAgency_LOGO_white.svg" alt="Teacher digital Agency logo" width="74" />
         </NuxtLink>
